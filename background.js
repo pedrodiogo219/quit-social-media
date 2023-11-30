ca9aaaabb3166499a3755402cc97c91cde52b653
Pedro Diogo <p.diogo@eyeo.com>
2023-11-30 14:28:11 +0100

    redirecting every time we go to instagram

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-console.log("hello")
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+    var url = new URL(details.url);
+    if (url.hostname === "www.instagram.com" || url.hostname === "instagram.com") {
+        // Cancel the navigation if the URL is Instagram
+        chrome.tabs.update(details.tabId, {
+            url: "chrome-extension://"+chrome.runtime.id+"/hello.html"
+        });
+    }
+});
