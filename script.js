b88db9a6f2c775bda45a910a7365b476c027bec0
Pedro Diogo <p.diogo@eyeo.com>
2023-12-01 16:51:18 +0100

    add link that redirects to original page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 (async () => {
-    var destination = await chrome.storage.session.get("destination")
+    let destination = await chrome.storage.session.get("destination")
     console.log(destination.destination);
+
+    let unlockButton = document.getElementById("unlockButton");
+    unlockButton.href = destination.destination; 
 })()
