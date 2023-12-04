1f6989fb135f0e5be07f0aa545acf828eac52a02
Pedro Diogo <pedrodiogo219@gmail.com>
2023-12-04 22:50:49 +0100

    unlocking after 30s

--- script.js
+++ script.js
@@ -3,5 +3,24 @@
     console.log(destination.destination);
 
     let unlockButton = document.getElementById("unlockButton");
-    unlockButton.href = destination.destination; 
+    // Disable the button initially
+    unlockButton.classList.add('buttonDisabled');
+    unlockButton.href = '#';
+
+    let countdown = 30;
+    unlockButton.textContent = `Unlock (${countdown}s)`;
+
+    // Update the button text every second
+    let intervalCountdown = setInterval(() => {
+        countdown--;
+        unlockButton.textContent = `Unlock (${countdown}s)`;
+    }, 1000);
+
+    // Enable the button after 30 seconds
+    setTimeout(() => {
+        clearInterval(intervalCountdown);
+        unlockButton.classList.remove('buttonDisabled');
+        unlockButton.href = destination.destination;
+        unlockButton.textContent = 'Unlock';
+    }, countdown*1000);
 })()
